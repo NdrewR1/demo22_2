cbaa839284f12cc0fa09e43fe8c59c185cbd34d4
unknown <andrewaruan@gmail.com>
2024-11-05 08:24:12 +0700

    Add file1.java and file2.java

---
+++ file1.java
@@ -0,0 +1,5 @@
+public class file1{
+    public void print(){
+        System.out.println("this is file1");
+    }
+}
---
+++ file2.java
@@ -0,0 +1,5 @@
+public class file2 {
+    public void print(){
+        System.out.println("this is file2");
+    }
+}
