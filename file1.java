8da710fe5529a6c0664276ef394bf1b14adf428c
unknown <andrewaruan@gmail.com>
2024-11-05 08:41:10 +0700

    Added new method on both file1.java and file2.java

--- file1.java
+++ file1.java
@@ -1,5 +1,9 @@
 public class file1{
     public void print(){
-        System.out.println("this is file1");
+        System.out.println("this is not file2");
+    }
+
+    public int methodMinus(int a, int b){
+        return a - b;
     }
 }
--- file2.java
+++ file2.java
@@ -2,4 +2,8 @@ public class file2 {
     public void print(){
         System.out.println("this is file2");
     }
+
+    public int methodPlus(int a, int b){
+        return a + b;
+    }
 }
